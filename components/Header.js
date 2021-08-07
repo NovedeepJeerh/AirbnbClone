60a9f3b591720a126fe57d8c18e0b76a9c413bde
Nove_Jeerh <68063461+NovedeepJeerh@users.noreply.github.com>
2021-08-07 13:43:17 +0530

    initial

--- components/Header.js
+++ components/Header.js
@@ -42,7 +42,7 @@ function Header({ placeholder }) {
 
     
     return (
-        <header className="sticky top-0 z-50 grid grid-cols-3 bg-black-400  text-white shadow-md p-5 md:px-10">
+        <header className="sticky top-0 z-50 grid grid-cols-3 bg-black  text-white shadow-md p-5 md:px-10">
             {/* Left */}
             <div onClick={() => router.push("/")} 
             className="relative flex items-center h-10 cursor-pointer my-auto">
